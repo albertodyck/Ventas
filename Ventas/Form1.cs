268b9d1e1910cca60490980635ab6a1e7f340298
albertodyck <albertodyck@hotmail.com>
2021-07-24 15:20:38 -0700

    Agregar archivos de proyecto.

---
+++ Ventas/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ventas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                LogicaNegocios.Empleado empleado = new LogicaNegocios.Empleado();
+
+                if (empleado.Login("Antonio", "Password"))
+                {
+                    MessageBox.Show("Conexion Satisfactoria");
+                }
+                else
+                {
+                    throw new Exception("Error de Conexión");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
